24789000ee14dad8302591b0f318692ef5452cf5
Александр <62289323+busyhedg03@users.noreply.github.com>
2023-02-17 11:54:07 +0700

    Add files via upload

--- remember_c.c
+++ remember_c.c
@@ -4,15 +4,24 @@
 
 #define PI (3.141592653589793)
 
-void fill_array(double* arr, long long size) {
-#pragma acc kernels
+double fill_array(double* arr, long long size) {
+	double sum = 0;
+	double temp = 2 * PI / size;
+#pragma acc data create(array[:size]) copy(sum) copyin(temp)
 	{
+#pragma acc kernels
 		for (long long i = 0; i < size; i++)
 		{
-			double x = i * 2 * PI / size;
+			double x = i * temp;
 			arr[i] = sin(x);
 		}
+#pragma acc kernels
+		for (long long i = 0; i < size; i++)
+		{
+			sum += arr[i];
+		}
 	}
+	return sum;
 }
 
 void print_array(double* arr, int size) {
@@ -27,7 +36,8 @@ int main()
 {
 #define size 1e7
 	double* arr = (double*)malloc(size * sizeof(double));
-	fill_array(arr, size);
+	double sum = fill_array(arr, size);
 	print_array(arr, size);
+	printf("Sum = %f", sum);
 	free(arr);
 }
